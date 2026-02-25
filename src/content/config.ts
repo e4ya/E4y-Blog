66296c93f195939e8e383baa8c5e64f4711ce4b9
e4ya <cao114514@yeah.net>
2026-02-25 22:08:58 +0800

    修复构建报错

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,34 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, type SchemaContext, z } from "astro:content";
 
 const postsCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		published: z.date(),
-		updated: z.date().optional(),
-		draft: z.boolean().optional().default(false),
-		description: z.string().optional().default(""),
-		image: z.string().optional().default(""),
-		tags: z.array(z.string()).optional().default([]),
-		category: z.string().optional().nullable().default(""),
-		lang: z.string().optional().default(""),
+	schema: ({ image }: SchemaContext) =>
+		z.object({
+			title: z.string(),
+			published: z.date(),
+			updated: z.date().optional(),
+			draft: z.boolean().optional().default(false),
+			description: z.string().optional().default(""),
+			image: image().optional(),
+			tags: z.array(z.string()).optional().default([]),
+			category: z.string().optional().nullable().default(""),
+			lang: z.string().optional().default(""),
 
-		/* For internal use */
-		prevTitle: z.string().default(""),
-		prevSlug: z.string().default(""),
-		nextTitle: z.string().default(""),
-		nextSlug: z.string().default(""),
-	}),
+			/* For internal use */
+			prevTitle: z.string().default(""),
+			prevSlug: z.string().default(""),
+			nextTitle: z.string().default(""),
+			nextSlug: z.string().default(""),
+		}),
 });
+
 const specCollection = defineCollection({
 	schema: z.object({}),
 });
-export const collections = {
+
+export const collections: {
+	posts: any;
+	spec: any;
+} = {
 	posts: postsCollection,
 	spec: specCollection,
 };
